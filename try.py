e023fd0edc31295f4a11901c64c236de26aa7eee
johnponce32 <john.ponce00032@gmail.com>
2020-10-02 13:32:22 +0800

    Initial commit

---
+++ try.py
@@ -0,0 +1,14 @@
+
+
+
+#This is a simple adder function
+#another changes
+
+
+def func_add(a, b):
+	return a + b
+
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+
+print(func_add(num1, num2))
